993980eff4c1de7381b3e298f96d707979f9bc3a
Luke Newell <p1322187x@myemail.dmu.ac.uk>
2017-03-01 21:48:52 +0000

    Fixed directories and Bullet project now builds

--- external/Bullet2.86/Extras/GIMPACTUtils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/FinalYearProject1.0/HavocSpeed/external/bullet-2.86.1/Extras/GIMPACTUtils
-# Build directory: C:/FinalYearProject1.0/HavocSpeed/external/Bullet2.86/Extras/GIMPACTUtils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
